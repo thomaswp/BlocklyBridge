ec695c52c46c4ba3b2937e21c401ead808154e6e
Thomas Price <thomaswprice1@gmail.com>
2022-06-07 12:10:49 -0400

    Formatter websocketcontroller and found a firefox bug

--- WebsocketServer.cs
+++ WebsocketServer.cs
@@ -88,38 +88,39 @@ namespace BlocklyBridge
                             }
                         }
 
-                        if (!client.Connected || !stream.DataAvailable || client.Available < 3)
+                        int available = client.Available;
+                        if (!client.Connected || !stream.DataAvailable || available < 3)
                         {
                             continue;
                         }
 
-                        byte[] bytes = new byte[client.Available];
-                        stream.Read(bytes, 0, client.Available);
-                    //ByteArrayToFile("Test.txt", bytes);
-                    // string s = Encoding.UTF8.GetString(bytes);
-                    string s = Encoding.ASCII.GetString(bytes);
-                    //Debug.Log("Receiving message: '" + s + "'");
-                    //Debug.Log(string.Join(",", bytes));
+                        byte[] bytes = new byte[available];
+                        stream.Read(bytes, 0, available);
+                        //ByteArrayToFile("Test.txt", bytes);
+                        // string s = Encoding.UTF8.GetString(bytes);
+                        string s = Encoding.ASCII.GetString(bytes);
+                        //Debug.Log("Receiving message: '" + s + "'");
+                        //Debug.Log(string.Join(",", bytes));
 
-                    if (Regex.IsMatch(s, "^GET", RegexOptions.IgnoreCase))
+                        if (Regex.IsMatch(s, "^GET", RegexOptions.IgnoreCase))
                         {
                             Logger.Log(string.Format("=====Handshaking from client=====\n{0}", s));
 
-                        // 1. Obtain the value of the "Sec-WebSocket-Key" request header without any leading or trailing whitespace
-                        // 2. Concatenate it with "258EAFA5-E914-47DA-95CA-C5AB0DC85B11" (a special GUID specified by RFC 6455)
-                        // 3. Compute SHA-1 and Base64 hash of the new value
-                        // 4. Write the hash back as the value of "Sec-WebSocket-Accept" response header in an HTTP response
-                        string swk = Regex.Match(s, "Sec-WebSocket-Key: (.*)").Groups[1].Value.Trim();
+                            // 1. Obtain the value of the "Sec-WebSocket-Key" request header without any leading or trailing whitespace
+                            // 2. Concatenate it with "258EAFA5-E914-47DA-95CA-C5AB0DC85B11" (a special GUID specified by RFC 6455)
+                            // 3. Compute SHA-1 and Base64 hash of the new value
+                            // 4. Write the hash back as the value of "Sec-WebSocket-Accept" response header in an HTTP response
+                            string swk = Regex.Match(s, "Sec-WebSocket-Key: (.*)").Groups[1].Value.Trim();
                             string swka = swk + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
                             byte[] swkaSha1 = System.Security.Cryptography.SHA1.Create().ComputeHash(Encoding.UTF8.GetBytes(swka));
                             string swkaSha1Base64 = Convert.ToBase64String(swkaSha1);
 
-                        // HTTP/1.1 defines the sequence CR LF as the end-of-line marker
-                        byte[] response = Encoding.UTF8.GetBytes(
-                                "HTTP/1.1 101 Switching Protocols\r\n" +
-                                "Connection: Upgrade\r\n" +
-                                "Upgrade: websocket\r\n" +
-                                "Sec-WebSocket-Accept: " + swkaSha1Base64 + "\r\n\r\n");
+                            // HTTP/1.1 defines the sequence CR LF as the end-of-line marker
+                            byte[] response = Encoding.UTF8.GetBytes(
+                                    "HTTP/1.1 101 Switching Protocols\r\n" +
+                                    "Connection: Upgrade\r\n" +
+                                    "Upgrade: websocket\r\n" +
+                                    "Sec-WebSocket-Accept: " + swkaSha1Base64 + "\r\n\r\n");
 
                             stream.Write(response, 0, response.Length);
 
@@ -130,12 +131,13 @@ namespace BlocklyBridge
                             bool fin = (bytes[0] & 0b10000000) != 0,
                                 mask = (bytes[1] & 0b10000000) != 0; // must be true, "All messages from the client to the server have this bit set"
 
-                        int opcode = bytes[0] & 0b00001111, // expecting 1 - text message
-                                msglen = bytes[1] - 128, // & 0111 1111
-                                offset = 2;
+                            int opcode = bytes[0] & 0b00001111, // expecting 1 - text message
+                                    msglen = bytes[1] - 128, // & 0111 1111
+                                    offset = 2;
 
                             if (msglen == 126)
                             {
+                                // TODO: Hits a bug with firefox that leads to a message length longer than bytes
                                 // was ToUInt16(bytes, offset) but the result is incorrect
                                 msglen = BitConverter.ToUInt16(new byte[] { bytes[3], bytes[2] }, 0);
                                 offset = 4;
@@ -199,7 +201,8 @@ namespace BlocklyBridge
                     //Debug.Log(offset + "/" + messageBytes.Length);
                     offset += length;
                     continuation = true;
-                } catch
+                }
+                catch
                 {
                     Logger.Warn("Error writing to stream; dropping message.");
                     break;
